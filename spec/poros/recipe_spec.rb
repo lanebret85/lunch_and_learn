e6ee8b7efbba4035ca6a9eeb77b08a33fe0867e0
Lane Bretschneider <lanebret85@gmail.com>
2023-11-12 16:25:02 -0800

    refactor: change format of test to account for change in the way that country attribute gets assigned to recipe poros now

--- spec/poros/recipe_spec.rb
+++ spec/poros/recipe_spec.rb
@@ -3,18 +3,18 @@ require "rails_helper"
 RSpec.describe Recipe do
   describe "existance" do
     it "exists and has readable attributes" do
-      recipe = Recipe.new(
-        {
-          label: "Chicken Nuggets",
-          url: "www.chicken_nuggets.com",
-          image: "lookatthechickennuggets.jpg"
-        }
-      )
+      recipe_info = {
+        label: "Chicken Nuggets",
+        url: "www.chicken_nuggets.com",
+        image: "lookatthechickennuggets.jpg"
+      }
+
+      recipe = Recipe.new(recipe_info, "Nuggieland")
 
       expect(recipe.id).to be(nil)
       expect(recipe.title).to eq("Chicken Nuggets")
       expect(recipe.url).to eq("www.chicken_nuggets.com")
-      expect(recipe.country).to eq("")
+      expect(recipe.country).to eq("Nuggieland")
       expect(recipe.image).to eq("lookatthechickennuggets.jpg")
     end
   end
