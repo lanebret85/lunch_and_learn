42c8703ca6d5e7eef60fb30e88749df4e6a0f96a
Lane Bretschneider <lanebret85@gmail.com>
2023-11-12 16:28:28 -0800

    refactor: change the way that country attribute gets assigned and remove attr_accessor that is no longer necessary

--- app/poros/recipe.rb
+++ app/poros/recipe.rb
@@ -2,14 +2,14 @@ class Recipe
   attr_reader :id,
               :title,
               :url,
-              :image
-  attr_accessor :country
+              :image,
+              :country
 
-  def initialize(recipe_info)
+  def initialize(recipe_info, country)
     @id = nil
     @title = recipe_info[:label]
     @url = recipe_info[:url]
-    @country = ""
+    @country = country
     @image = recipe_info[:image]
   end
 end
