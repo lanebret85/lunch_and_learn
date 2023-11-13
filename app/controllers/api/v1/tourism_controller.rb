b93b7c719d27c982d4e1182d915f3f9bba858da7
Lane Bretschneider <lanebret85@gmail.com>
2023-11-13 10:49:00 -0800

    feat: add error handling and sad path condition to account for Places API data not found or invalid country name

--- app/controllers/api/v1/tourism_controller.rb
+++ app/controllers/api/v1/tourism_controller.rb
@@ -1,6 +1,20 @@
 class Api::V1::TourismController < ApplicationController
   def index
-    tourist_sites = TourismFacade.new.get_tourist_sites(params[:country])
-    render json: TouristSiteSerializer.new(tourist_sites)
+    begin
+      tourist_sites = TourismFacade.new.get_tourist_sites(params[:country])
+      if tourist_sites.empty?
+        render json: {
+          status: 404,
+          error: "Tourist sites not found"
+        }
+      else
+        render json: TouristSiteSerializer.new(tourist_sites)
+      end
+    rescue => e
+      render json: {
+        status: 404,
+        error: "Invalid country name"
+      }
+    end
   end
 end
