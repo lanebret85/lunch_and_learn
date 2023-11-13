ac906c31b6433e5f9ad633e40735f1acd2276bf0
Lane Bretschneider <lanebret85@gmail.com>
2023-11-13 09:42:46 -0800

    feat: add route for get '/api/v1/tourist_sites'

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
 
   get "/api/v1/recipes", to: "api/v1/recipes#index"
   get "/api/v1/learning_resources", to: "api/v1/resources#index"
+
+  get "/api/v1/tourist_sites", to: "api/v1/tourism#index"
 end
